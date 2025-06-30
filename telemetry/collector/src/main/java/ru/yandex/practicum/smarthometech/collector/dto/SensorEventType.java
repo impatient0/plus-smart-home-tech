a3bfa2d847e93a45fdd6f44d28348d2643ba18f3
Pepe Ronin <ivanpetrovskiy98@gmail.com>
2025-06-30 21:08:22 +0300

    rename enums to be consistent with specs (again)

--- telemetry/collector/src/main/java/ru/yandex/practicum/smarthometech/collector/dto/SensorEventType.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/smarthometech/collector/dto/SensorEventType.java
@@ -2,8 +2,8 @@ package ru.yandex.practicum.smarthometech.collector.dto;
 
 public enum SensorEventType {
     MOTION_SENSOR,
-    TEMPERATURE_SENSOR,
-    LIGHT_SENSOR,
-    CLIMATE_SENSOR,
-    SWITCH_SENSOR
+    TEMPERATURE_SENSOR_EVENT,
+    LIGHT_SENSOR_EVENT,
+    CLIMATE_SENSOR_EVENT,
+    SWITCH_SENSOR_EVENT
 }
