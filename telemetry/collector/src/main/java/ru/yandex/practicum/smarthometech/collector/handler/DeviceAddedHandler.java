b39ecc55e25337a0d11eea43d83f77805d6a334c
Pepe Ronin <ivanpetrovskiy98@gmail.com>
2025-07-09 02:02:45 +0300

    handler now skips messages with unspecified device type

--- telemetry/collector/src/main/java/ru/yandex/practicum/smarthometech/collector/handler/DeviceAddedHandler.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/smarthometech/collector/handler/DeviceAddedHandler.java
@@ -2,7 +2,9 @@ package ru.yandex.practicum.smarthometech.collector.handler;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import ru.yandex.practicum.grpc.telemetry.event.HubEventProto;
 import ru.yandex.practicum.grpc.telemetry.event.HubEventProto.PayloadCase;
 import ru.yandex.practicum.smarthometech.collector.kafka.TelemetryKafkaProducer;
 import ru.yandex.practicum.smarthometech.collector.mapper.EventMapper;
@@ -10,6 +12,7 @@ import ru.yandex.practicum.smarthometech.collector.mapper.EventMapper;
 @Component
 @RequiredArgsConstructor
 @Getter
+@Slf4j
 public class DeviceAddedHandler implements HubEventHandler {
 
     private final EventMapper mapper;
@@ -18,5 +21,20 @@ public class DeviceAddedHandler implements HubEventHandler {
     @Override
     public PayloadCase getHandledType() {
         return PayloadCase.DEVICE_ADDED;
+    }
+
+    @Override
+    public void process(HubEventProto protoEvent) {
+        var avroEvent = getMapper().toAvro(protoEvent);
+
+        if (avroEvent != null) {
+            log.info("Processing valid DEVICE_ADDED event for hubId: {}", protoEvent.getHubId());
+            getProducer().sendHubEvent(avroEvent);
+        } else {
+            log.warn(
+                "Skipping invalid DEVICE_ADDED event for hubId: {}. Reason: Unknown or "
+                    + "unsupported device type.",
+                protoEvent.getHubId());
+        }
     }
 }
