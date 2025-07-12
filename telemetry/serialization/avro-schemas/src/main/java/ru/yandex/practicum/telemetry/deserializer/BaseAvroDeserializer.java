7e9d2838dc078125278d50f95537a5900f790e8f
Pepe Ronin <ivanpetrovskiy98@gmail.com>
2025-07-12 16:04:31 +0300

    create/rename base and specific serializers/deserializers

---
+++ telemetry/serialization/avro-schemas/src/main/java/ru/yandex/practicum/telemetry/deserializer/BaseAvroDeserializer.java
@@ -0,0 +1,45 @@
+package ru.yandex.practicum.telemetry.deserializer;
+
+import java.io.IOException;
+import java.util.Arrays;
+import org.apache.avro.Schema;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.Decoder;
+import org.apache.avro.io.DecoderFactory;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.commons.lang3.SerializationException;
+import org.apache.kafka.common.serialization.Deserializer;
+
+public class BaseAvroDeserializer<T extends SpecificRecordBase> implements Deserializer<T> {
+
+    private final DatumReader<T> datumReader;
+
+    private final DecoderFactory decoderFactory;
+
+    public BaseAvroDeserializer(Schema schema) {
+        this(DecoderFactory.get(), schema);
+    }
+
+    public BaseAvroDeserializer(DecoderFactory decoderFactory, Schema schema) {
+        this.decoderFactory = decoderFactory;
+        this.datumReader = new SpecificDatumReader<>(schema);
+    }
+
+    @Override
+    public T deserialize(String topic, byte[] data) {
+        if (data == null) {
+            return null;
+        }
+
+        try {
+            Decoder decoder = decoderFactory.binaryDecoder(data, null);
+
+            return datumReader.read(null, decoder);
+        } catch (IOException e) {
+            throw new SerializationException(
+                "Error deserializing Avro message for topic " + topic + ". Data: "
+                    + Arrays.toString(data), e);
+        }
+    }
+}
---
+++ telemetry/serialization/avro-schemas/src/main/java/ru/yandex/practicum/telemetry/serializer/SensorSnapshotAvroSerializer.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.telemetry.serializer;
+
+import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
+
+public class SensorSnapshotAvroSerializer extends BaseAvroSerializer<SensorsSnapshotAvro> {
+
+}
