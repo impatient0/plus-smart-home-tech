a56fcdc8167c64477ff88ce7ce9536a6496f8b82
Pepe Ronin <ivanpetrovskiy98@gmail.com>
2025-08-04 19:33:38 +0300

    feat(interaction-api): implement DTOs and Feign clients

---
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/smarthometech/commerce/api/client/ShoppingCartClient.java
@@ -0,0 +1,34 @@
+package ru.yandex.practicum.smarthometech.commerce.api.client;
+
+import java.util.List;
+import java.util.Map;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.yandex.practicum.commerce.api.dto.cart.ChangeProductQuantityRequest;
+import ru.yandex.practicum.commerce.api.dto.warehouse.ShoppingCartDto;
+
+@FeignClient(name = "shopping-cart", path = "/api/v1/shopping-cart")
+public interface ShoppingCartClient {
+
+    @GetMapping
+    ShoppingCartDto getCart(@RequestParam String username);
+
+    @PutMapping
+    ShoppingCartDto addItem(@RequestParam String username, @RequestBody Map<String, Integer> items);
+
+    @DeleteMapping
+    void deactivateCart(@RequestParam String username);
+
+    @PostMapping("/remove")
+    ShoppingCartDto removeItem(@RequestParam String username, @RequestBody List<String> items);
+
+    @PostMapping("/change-quantity")
+    ShoppingCartDto changeItemQuantity(@RequestParam String username, @RequestBody
+        ChangeProductQuantityRequest request);
+
+}
---
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/smarthometech/commerce/api/client/ShoppingStoreClient.java
@@ -0,0 +1,36 @@
+package ru.yandex.practicum.smarthometech.commerce.api.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.yandex.practicum.commerce.api.dto.store.ProductDto;
+import ru.yandex.practicum.commerce.api.dto.store.SetProductQuantityStateRequest;
+
+@FeignClient(name = "shopping-store", path = "/api/v1/shopping-store")
+public interface ShoppingStoreClient {
+
+    @GetMapping
+    Page<ProductDto> getProducts(@RequestParam("category") String category, Pageable pageable);
+
+    @PutMapping
+    ProductDto addNewProduct(@RequestBody ProductDto product);
+
+    @PostMapping
+    ProductDto updateProduct(@RequestBody ProductDto product);
+
+    @PostMapping("/removeProductFromStore")
+    Boolean removeProduct(@RequestBody String productId);
+
+    @PostMapping("/quantityState")
+    Boolean setStatus(@RequestBody SetProductQuantityStateRequest request);
+
+    @GetMapping("/{productId}")
+    ProductDto getProduct(@PathVariable String productId);
+
+}
