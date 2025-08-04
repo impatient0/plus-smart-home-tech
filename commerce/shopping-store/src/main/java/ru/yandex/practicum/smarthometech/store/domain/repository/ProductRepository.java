9be1b2f4fafd841a2f878460900833d4fdf1eb11
Pepe Ronin <ivanpetrovskiy98@gmail.com>
2025-08-04 21:16:46 +0300

    feat(shopping-store): add Product entity and repository

---
+++ commerce/shopping-store/src/main/java/ru/yandex/practicum/smarthometech/store/domain/repository/ProductRepository.java
@@ -0,0 +1,14 @@
+package ru.yandex.practicum.smarthometech.store.domain.repository;
+
+import java.util.UUID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.commerce.api.dto.store.ProductCategory;
+import ru.yandex.practicum.smarthometech.store.domain.entity.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+    Page<Product> findByProductCategory(ProductCategory category, Pageable pageable);
+}
