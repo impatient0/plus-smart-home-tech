fea1d053f47797a164b47f37d36a8e6548f302df
Pepe Ronin <ivanpetrovskiy98@gmail.com>
2025-08-06 00:19:50 +0300

    build(db): isolate services with dedicated database schemas and users

--- init.sql
+++ init.sql
@@ -1,54 +1,83 @@
--- создаём таблицу scenarios
-CREATE TABLE IF NOT EXISTS scenarios (
+-- создаём схемы
+CREATE SCHEMA IF NOT EXISTS analyzer;
+CREATE SCHEMA IF NOT EXISTS shopping_cart;
+CREATE SCHEMA IF NOT EXISTS shopping_store;
+CREATE SCHEMA IF NOT EXISTS warehouse;
+
+-- создаём пользователей
+CREATE ROLE analyzer_user WITH LOGIN PASSWORD 'analyzer_password';
+CREATE ROLE store_user WITH LOGIN PASSWORD 'store_password';
+CREATE ROLE cart_user WITH LOGIN PASSWORD 'cart_password';
+CREATE ROLE warehouse_user WITH LOGIN PASSWORD 'warehouse_password';
+
+-- выдаём пользователям привилегии на их схемах
+GRANT ALL PRIVILEGES ON SCHEMA analyzer TO analyzer_user;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA analyzer TO analyzer_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA analyzer GRANT ALL ON TABLES TO analyzer_user;
+
+GRANT ALL PRIVILEGES ON SCHEMA shopping_cart TO cart_user;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA shopping_cart TO cart_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA shopping_cart GRANT ALL ON TABLES TO cart_user;
+
+GRANT ALL PRIVILEGES ON SCHEMA shopping_store TO store_user;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA shopping_store TO store_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA shopping_store GRANT ALL ON TABLES TO store_user;
+
+GRANT ALL PRIVILEGES ON SCHEMA warehouse TO warehouse_user;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA warehouse TO warehouse_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA warehouse GRANT ALL ON TABLES TO warehouse_user;
+
+-- создаём таблицу scenarios в схеме analyzer
+CREATE TABLE IF NOT EXISTS analyzer.scenarios (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     hub_id VARCHAR,
     name VARCHAR,
     UNIQUE(hub_id, name)
 );
 
--- создаём таблицу sensors
-CREATE TABLE IF NOT EXISTS sensors (
+-- создаём таблицу sensors в схеме analyzer
+CREATE TABLE IF NOT EXISTS analyzer.sensors (
     id VARCHAR PRIMARY KEY,
     hub_id VARCHAR
 );
 
--- создаём таблицу conditions
-CREATE TABLE IF NOT EXISTS conditions (
+-- создаём таблицу conditions в схеме analyzer
+CREATE TABLE IF NOT EXISTS analyzer.conditions (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     type VARCHAR,
     operation VARCHAR,
     value INTEGER
 );
 
--- создаём таблицу actions
-CREATE TABLE IF NOT EXISTS actions (
+-- создаём таблицу actions в схеме analyzer
+CREATE TABLE IF NOT EXISTS analyzer.actions (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     type VARCHAR,
     value INTEGER
 );
 
--- создаём таблицу scenario_conditions, связывающую сценарий, датчик и условие активации сценария
-CREATE TABLE IF NOT EXISTS scenario_conditions (
-    scenario_id BIGINT REFERENCES scenarios(id),
-    sensor_id VARCHAR REFERENCES sensors(id),
-    condition_id BIGINT REFERENCES conditions(id),
+-- создаём таблицу scenario_conditions в схеме analyzer, связывающую сценарий, датчик и условие активации сценария
+CREATE TABLE IF NOT EXISTS analyzer.scenario_conditions (
+    scenario_id BIGINT REFERENCES analyzer.scenarios(id),
+    sensor_id VARCHAR REFERENCES analyzer.sensors(id),
+    condition_id BIGINT REFERENCES analyzer.conditions(id),
     PRIMARY KEY (scenario_id, sensor_id, condition_id)
 );
 
--- создаём таблицу scenario_actions, связывающую сценарий, датчик и действие, которое нужно выполнить при активации сценария
-CREATE TABLE IF NOT EXISTS scenario_actions (
-    scenario_id BIGINT REFERENCES scenarios(id),
-    sensor_id VARCHAR REFERENCES sensors(id),
-    action_id BIGINT REFERENCES actions(id),
+-- создаём таблицу scenario_actions в схеме analyzer, связывающую сценарий, датчик и действие, которое нужно выполнить при активации сценария
+CREATE TABLE IF NOT EXISTS analyzer.scenario_actions (
+    scenario_id BIGINT REFERENCES analyzer.scenarios(id),
+    sensor_id VARCHAR REFERENCES analyzer.sensors(id),
+    action_id BIGINT REFERENCES analyzer.actions(id),
     PRIMARY KEY (scenario_id, sensor_id, action_id)
 );
 
--- создаём функцию для проверки, что связываемые сценарий и датчик работают с одним и тем же хабом
-CREATE OR REPLACE FUNCTION check_hub_id()
+-- создаём функцию для проверки, что связываемые сценарий и датчик работают с одним и тем же хабом, в схеме analyzer
+CREATE OR REPLACE FUNCTION analyzer.check_hub_id()
 RETURNS TRIGGER AS
 '
 BEGIN
-    IF (SELECT hub_id FROM scenarios WHERE id = NEW.scenario_id) != (SELECT hub_id FROM sensors WHERE id = NEW.sensor_id) THEN
+    IF (SELECT hub_id FROM analyzer.scenarios WHERE id = NEW.scenario_id) != (SELECT hub_id FROM analyzer.sensors WHERE id = NEW.sensor_id) THEN
         RAISE EXCEPTION ''Hub IDs do not match for scenario_id % and sensor_id %'', NEW.scenario_id, NEW.sensor_id;
     END IF;
     RETURN NEW;
@@ -58,12 +87,12 @@ LANGUAGE plpgsql;
 
 -- создаём триггер, проверяющий, что «условие» связывает корректные сценарий и датчик
 CREATE OR REPLACE TRIGGER tr_bi_scenario_conditions_hub_id_check
-BEFORE INSERT ON scenario_conditions
+BEFORE INSERT ON analyzer.scenario_conditions
 FOR EACH ROW
-EXECUTE FUNCTION check_hub_id();
+EXECUTE FUNCTION analyzer.check_hub_id();
 
 -- создаём триггер, проверяющий, что «действие» связывает корректные сценарий и датчик
 CREATE OR REPLACE TRIGGER tr_bi_scenario_actions_hub_id_check
-BEFORE INSERT ON scenario_actions
+BEFORE INSERT ON analyzer.scenario_actions
 FOR EACH ROW
-EXECUTE FUNCTION check_hub_id();
+EXECUTE FUNCTION analyzer.check_hub_id();
