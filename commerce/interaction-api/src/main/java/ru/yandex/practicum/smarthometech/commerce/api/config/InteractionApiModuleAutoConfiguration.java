b5b0e1d3493ee4c43f0062a5c0185c517f6d983b
Pepe Ronin <ivanpetrovskiy98@gmail.com>
2025-08-06 13:15:01 +0300

    feat(interaction-api): add spring boot autoconfiguration

---
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/smarthometech/commerce/api/config/InteractionApiModuleAutoConfiguration.java
@@ -0,0 +1,9 @@
+package ru.yandex.practicum.smarthometech.commerce.api.config;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@AutoConfiguration
+@ComponentScan("ru.yandex.practicum.smarthometech.commerce.api.mapper")
+public class InteractionApiModuleAutoConfiguration {
+}
